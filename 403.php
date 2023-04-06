dc915e0893981ee7a29d21b203467ca96d164111
molanp <782552619@qq.com>
2023-04-06 11:11:58 +0800

    更新伪静态

---
+++ 403.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>403 Forbidden</title>
+</head>
+<body>
+    <center>
+        <h1>403 Forbidden</h3>
+        <p>You don't have permission to access this resource.</p>
+        <hr>
+        <?php echo date("Y-m-d h:i:s")?>
+    </center>
+</body>
+</html>
