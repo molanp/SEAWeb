a1f8c7b4eb6702e70fa8a52e2be5c6c75ef62f3f
molanp <782552619@qq.com>
2023-06-03 17:03:57 +0800

    简化api参数设置，详见文档re_add和re_par

--- api/lanzou/index.php
+++ api/lanzou/index.php
@@ -6,22 +6,9 @@ $api_profile='在线解析蓝奏云下载链接，获取文件直链';
 $version='1.0';
 $author='molanp';
 $type='一些工具';
-$api_address="
-|请求方式|请求地址|说明|
-|---|---|---|
-|get|[/api/lanzou](/api/lanzou)|-|";
-$request_par='
-|参数|说明|
-|---|---|
-|*`url`|蓝奏云文件分享链接|
-|`pwd`|文件的下载密码(无密码请留空)|
-|`down`|是否直接跳转下载，是则填`true`|';
-$return_par='
-|基础数据|说明|
-|---|---|
-|`name`|文件名称|
-|`filesize`|文件大小|
-|`url`|文件下载地址|';
+$api_address=re_add(['GET'],['/api/lanzou'],['-']);
+$request_par=re_par(['*url','pwd','down'],['蓝奏云文件分享链接','文件的下载密码(无密码请留空)','是否直接跳转下载，是则填`true`']);
+$return_par=re_par(['name','filesize','url'],['文件名称','文件大小','文件下载地址']);
 if (handle_check()) {
     include_once('lanzou.php');
     $url = $_GET['url'] ?? NULL;
--- api/tian/index.php
+++ api/tian/index.php
@@ -6,18 +6,9 @@ $api_profile='一键获取舔狗日记';
 $version='1.0';
 $author='molanp';
 $type='一些工具';
-$api_address="
-|请求方式|请求地址|说明|
-|---|---|---|
-|get|[/api/tian](/api/tian)|-|";
-$request_par='
-|参数|说明|
-|---|---|
-|`None`|无需任何参数|';
-$return_par='
-|基础数据|说明|
-|---|---|
-|`data`|舔狗语录|';
+$api_address=re_add(['GET'],['/api/tian'],['-']);
+$request_par=re_par(['None'],['无需参数']);
+$return_par=re_par(['data'],['舔狗语录']);
 if (handle_check()) {
     include_once('tgrj.php');
     _return_($data[array_rand($data)]);
