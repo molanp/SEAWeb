80d971f3dd81f94fc4f60c6f71a6401f927b8d93
molanp <782552619@qq.com>
2023-07-24 22:55:23 +0800

    修改必填参数标记样式

---
+++ data/web.php
@@ -0,0 +1,19 @@
+{
+    "account": {
+        "username": "admin",
+        "password": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
+    },
+    "web": {
+        "record": "",
+        "index_title": "SEAWeb",
+        "copyright": "",
+        "index_description": "这是网站简介，这里支持*MarkDown*语法",
+        "notice": {
+            "data": "> **这里也支持markdown语法**\n\n欢迎使用SEAWeb，本模板由[molanp](https:\/\/github.com\/molanp)开发与维护。目前正在不断完善~\n如果你觉得这个API有什么不完善的地方或者说你有什么更好的想♂法，可以在[issues](https:\/\/github.com\/molanp\/easyapi_wesbite\/issues)上提出建议",
+            "latesttime": "2023-07-24"
+        },
+        "keywords": "API,api",
+        "links": "[GitHub](https:\/\/github.com\/molanp\/SEAWeb)\n[Issues](https:\/\/github.com\/molanp\/SEAWeb\/issues)\n[开发指南](https:\/\/molanp.github.io\/SEAWeb_docs)"
+    },
+    "__system__": false
+}
