8d3e7ebffc9b28b2ed3f06ddbf2405f8a0691a1b
molanp <molanp>
2023-12-21 22:45:07 +0800

    支持一键返回顶部，修复主题选择器

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -10,17 +10,20 @@ window
 function theme_light() {
     deleteCookie("theme");
     mdui.setTheme("light");
+    $("#theme_select").val("light");
     $("#theme").attr("icon", "light_mode--outlined");
 }
 
 function theme_dark() {
     setCookie("theme", 1);
     mdui.setTheme("dark");
+    $("#theme_select").val("dark");
     $("#theme").attr("icon", "dark_mode--outlined");
 }
 
 function theme_auto() {
     deleteCookie("theme");
     mdui.setTheme("auto");
+    $("#theme_select").val("auto");
     $("#theme").attr("icon", "light_mode--outlined");
 }
